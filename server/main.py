aeb85b3e96065a7664624fa94ec88769f4b96043
Simoffsky <maks-kirsanov26@mail.ru>
2024-11-09 22:47:51 +1000

    add json compression

--- server/main.py
+++ server/main.py
@@ -19,12 +19,22 @@ async def create_upload_file(file: UploadFile):
 
     # т.к json очень большой то лучше отдавать его в виде ссылки на файл
     json_filename = f"static/{hash}.json"
+    # data = {
+    #     "FrL": data[:, 0].tolist(),
+    #     "FrR": data[:, 1].tolist(),
+    #     "OcR": data[:, 2].tolist(),
+    #     "classes": data[:, 3].tolist()
+    # }
+    
+    # постарался сжать данные
+    import numpy as np
     data = {
-        "FrL": data[:, 0].tolist(),
-        "FrR": data[:, 1].tolist(),
-        "OcR": data[:, 2].tolist(),
+        "FrL": np.round(data[:, 0], 6).tolist(),
+        "FrR": np.round(data[:, 1], 6).tolist(),
+        "OcR": np.round(data[:, 2], 6).tolist(),
         "classes": data[:, 3].tolist()
     }
+    
     import json
     with open("server/"+json_filename, "w") as f:
         f.write(json.dumps(data))
