de4838250df3910eec5c7238d676b21bb9627c0a
Simoffsky <maks-kirsanov26@mail.ru>
2024-11-09 22:06:10 +1000

    add edf saver in parser

--- parser/parser.py
+++ parser/parser.py
@@ -58,16 +58,56 @@ def parse_file(file_path: str) -> Tuple[np.ndarray, list, list, list]:
             i += 2
         else:
             i += 1  
-    # надо чтобы классы были int
-    classes = classes.astype(int)
+    
     data_with_classes = np.column_stack((data, classes))
     
 
     return data_with_classes, swd_annotation, is_annotation, ds_annotation
 
 
-# Сохраняем 3 сигнала (FrL, FrR, OcR) вместе с классами в csv файл
+# save_to_edf принимает на вход матрицу сигналов и классов и путь к файлу, в который нужно сохранить данные
+def save_to_edf(data_with_classes: np.ndarray, output_file: str) -> None:
+    signals = data_with_classes[:, :3].T  
+    classes = data_with_classes[:, 3] 
+
+    sampling_frequency = 400  
+    ch_names = ['FrL', 'FrR', 'OcR']
+    info = mne.create_info(ch_names, sfreq=sampling_frequency, ch_types=['eeg'] * 3)
+
+    raw = mne.io.RawArray(signals, info)
+
+    annotations = mne.Annotations(onset=[], duration=[], description=[])
+    i = 0
+    while i < len(classes):
+        class_label = classes[i]
+        if class_label != 0:
+            start = i
+            while i < len(classes) and classes[i] == class_label:
+                i += 1
+            end = i
+            onset_start = start / sampling_frequency
+            onset_end = end / sampling_frequency
+            # Добавляем аннотации начала и конца с нулевой длительностью
+            if class_label == 1:
+                description_start, description_end = 'swd1', 'swd2'
+            elif class_label == 2:
+                description_start, description_end = 'is1', 'is2'
+            elif class_label == 3:
+                description_start, description_end = 'ds1', 'ds2'
+ 
+            annotations.append(onset_start, 0, description_start)
+            annotations.append(onset_end, 0, description_end)
+        else:
+            i += 1
+
+
+    raw.set_annotations(annotations)
+    raw.export(output_file, fmt='edf')
+
+
+# save_to_csv сохраняет 3 сигнала (FrL, FrR, OcR) вместе с классами в csv файл
 def save_to_csv(data: np.ndarray, filename: str) -> None:
+
     header = 'FrL,FrR,OcR,Class'
     np.savetxt(filename, data, delimiter=',', header=header, comments='')
 
