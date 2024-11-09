0634dad6f27134d45e5e17e7ea80d865ed2d144a
Simoffsky <maks-kirsanov26@mail.ru>
2024-11-10 00:13:02 +1000

    add plotly json

--- server/main.py
+++ server/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI, UploadFile
 import hashlib
 from .ai import get_marked_edf
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="./server/static"), name="static")
 
@@ -42,20 +52,63 @@ async def create_upload_file(file: UploadFile):
          "classes": list(map(int, data[:, 3]))
     }
 
-    import msgpack
-    packed = msgpack.packb(data)
-    pack_filename = f"static/{hash}.msgpack"
-    import gzip
-    with gzip.open("server/" + pack_filename + ".gz", "wb") as f:
-        f.write(packed)
+    # import msgpack
+    # packed = msgpack.packb(data)
+    # pack_filename = f"static/{hash}.msgpack"
+    # import gzip
+    # with gzip.open("server/" + pack_filename + ".gz", "wb") as f:
+    #     f.write(packed)
 
-    import json
-    with open("server/"+json_filename, "w") as f:
-        f.write(json.dumps(data))
+    # import json
+    # with open("server/"+json_filename, "w") as f:
+    #     f.write(json.dumps(data))
+
+    import gzip
+
+    graph_json = create_plot(data)
+    with gzip.open("server/" + json_filename + ".gz", "wb") as f:
+        f.write(graph_json.encode())
+    
     resp = {
         "file": marked_edf_filename,
-        "json": json_filename,
-        "msgpack": pack_filename + ".gz"
+        "graph": json_filename
+        #"json": json_filename,
+        #"msgpack": pack_filename + ".gz"
     }
 
     return resp
+
+import plotly.graph_objects as go
+import plotly.express as px
+
+def create_plot(data):
+    # Преобразуем данные
+    FrL = data["FrL"]
+    FrR = data["FrR"]
+    OcR = data["OcR"]
+    classes = data["classes"]
+
+    # Создаем фигуру
+    fig = go.Figure()
+
+    # Для каждого класса (0, 1, 2, 3) добавляем scatter trace
+    colors = {0: "blue", 1: "green", 2: "red", 3: "orange"}
+    for class_label in [0, 1, 2, 3]:
+        class_indices = [i for i, c in enumerate(classes) if c == class_label]
+        fig.add_trace(go.Scatter(x=[FrL[i] for i in class_indices],
+                                 y=[FrR[i] for i in class_indices],
+                                 mode='markers',
+                                 name=f'Class {class_label}',
+                                 marker=dict(color=colors[class_label])))
+
+    # Настроим макет графика
+    fig.update_layout(
+        title="Signal Channels with Class Colors",
+        xaxis_title="FrL",
+        yaxis_title="FrR",
+        legend_title="Classes",
+        showlegend=True,
+    )
+    
+    # Возвращаем график в формате JSON
+    return fig.to_json()
