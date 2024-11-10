904dcea45eb45972f99b3a06ed7e98795bdf3d53
Simoffsky <maks-kirsanov26@mail.ru>
2024-11-10 13:48:07 +1000

    fix hardcoded file

--- server/ai.py
+++ server/ai.py
@@ -3,7 +3,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from parser.parser import parse_file, save_to_edf
 from .word import save_analytics_to_word
 def get_marked_edf(unmarked_filename, hash):
-    data, swd, is_, ds = parse_file(unmarked_filename)
+    hardcoded_file = "server/static/hardcoded.edf" # Добавить обработку нейронки
+    data, swd, is_, ds = parse_file(hardcoded_file)
 
     marked_filename = f"static/{hash}_marked.edf"
     analytics = save_to_edf(data, "server/"+marked_filename)
